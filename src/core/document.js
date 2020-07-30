5c846975b9b2ed016da2d9afe0923aa6ff27d5c6
Aki Sasaki <aki@mozilla.com>
2020-07-30 11:41:01 -0700

    may need to call from Page

--- src/core/document.js
+++ src/core/document.js
@@ -547,6 +547,7 @@ class PDFDocument {
       this.acroForm = this.catalog.catDict.get("AcroForm");
       if (this.acroForm) {
         this.xfa = this.acroForm.get("XFA");
+        this.acroForm.annotationFonts = [];
         const fields = this.acroForm.get("Fields");
         if ((!Array.isArray(fields) || fields.length === 0) && !this.xfa) {
           this.acroForm = null; // No fields and no XFA, so it's not a form.
@@ -857,6 +858,7 @@ class PDFDocument {
       "XObject",
       "Font",
     ];
+    // XXX not sure if this is useful. we may want to call this from Page ?
     const objectLoader = new ObjectLoader(this.DR, keys, xref);
     return objectLoader.load();
   }
