8c0cd5f081ffea847de6580559d650f20ab84d96
Aki Sasaki <aki@mozilla.com>
2020-08-05 13:14:15 -0700

    tie it into annotationfactory?

--- src/core/annotation.js
+++ src/core/annotation.js
@@ -50,13 +50,17 @@ class AnnotationFactory {
    */
   static create(xref, ref, pdfManager, idFactory) {
     return pdfManager.ensureDoc("acroForm").then(acroForm => {
-      return pdfManager.ensure(this, "_create", [
-        xref,
-        ref,
-        pdfManager,
-        idFactory,
-        acroForm,
-      ]);
+      return pdfManager
+        .ensureDoc("defaultAppearance")
+        .then(defaultAppearance => {
+          return pdfManager.ensure(this, "_create", [
+            xref,
+            ref,
+            pdfManager,
+            idFactory,
+            acroForm,
+          ]);
+        });
     });
   }
 
